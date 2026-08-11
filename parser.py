508a0df4c3bee68ff92a974e7a6be21cd18b51b0
Victor-Okenwa <okenwavictor003@gmail.com>
2026-08-11 20:20:42 +0100

    Fix formatting in parser.py by removing unnecessary whitespace

--- parser.py
+++ parser.py
@@ -1,5 +1,5 @@
 # Single file parsing Logic
-from pydantic import BaseModel, Field 
+from pydantic import BaseModel, Field
 
 #This model represents the analyzed statistics of a single Markdown file.
 class FileMetrics(BaseModel):
